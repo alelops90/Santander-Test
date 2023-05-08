0cb19095354a57638f6bfa891bb07506019122a4
Alexandre Martins <alexandre.martins@digitalpages.com.br>
2023-05-08 11:07:36 -0300

    Resolução do erro do swagger fox

--- src/main/java/com/teste/bank/BankApplication.java
+++ src/main/java/com/teste/bank/BankApplication.java
@@ -1,8 +1,11 @@
 package com.teste.bank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class BankApplication {
 
